54fb44e13a811c457d8669b97edfa37b9c8eb281
Georgi <radevg98@gmail.com>
2023-05-15 18:27:07 +0300

    Add details activity label and back icon

--- app/src/main/java/com/example/recipes_app/bindingadapters/RecipesRowBindings.kt
+++ app/src/main/java/com/example/recipes_app/bindingadapters/RecipesRowBindings.kt
@@ -1,16 +1,37 @@
 package com.example.recipes_app.bindingadapters
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.example.recipes_app.R
+import com.example.recipes_app.models.Result
+import com.example.recipes_app.ui.fragments.recipes.RecipesFragmentDirections
 
 class RecipesRowBindings {
 
     companion object {
+
+
+        @BindingAdapter("onRecipeClickListener")
+        @JvmStatic
+        fun onRecipeClickListener(recipeRowLayout: ConstraintLayout, result: Result) {
+            Log.d("onRecipeClickListener", "CALLED")
+            recipeRowLayout.setOnClickListener {
+                try {
+                    val action =
+                        RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+                    recipeRowLayout.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.d("onRecipeClickListener", e.toString())
+                }
+            }
+        }
 
         @BindingAdapter("loadImageFromUrl")
         @JvmStatic
--- app/src/main/java/com/example/recipes_app/ui/DetailsActivity.kt
+++ app/src/main/java/com/example/recipes_app/ui/DetailsActivity.kt
@@ -2,11 +2,28 @@ package com.example.recipes_app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
 import com.example.recipes_app.R
+import com.example.recipes_app.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
