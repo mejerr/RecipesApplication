33b376e8de69d159d3ab383e1efcf1fb78803de0
Georgi <radevg98@gmail.com>
2023-05-15 18:13:51 +0300

    Add design for activity details

---
+++ app/src/main/java/com/example/recipes_app/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recipes_app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recipes_app.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
