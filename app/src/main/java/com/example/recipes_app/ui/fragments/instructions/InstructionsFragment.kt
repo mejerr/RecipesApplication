c24ad5290c1879ce194cee12006173ea68dd0aec
Georgi <radevg98@gmail.com>
2023-05-15 23:49:55 +0300

    Add instructions page as opening a specific url given from the recipe request + small fixes

--- app/src/main/java/com/example/recipes_app/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/recipes_app/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,15 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.recipes_app.R
+import android.webkit.WebViewClient
+import com.example.recipes_app.databinding.FragmentInstructionsBinding
+import com.example.recipes_app.models.Result
+import com.example.recipes_app.util.Constants.Companion.RECIPE_RESULT_KEY
+import com.example.recipes_app.util.retrieveParcelable
 
 class InstructionsFragment : Fragment() {
+
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.retrieveParcelable(RECIPE_RESULT_KEY)
+
+        if (myBundle != null) {
+            binding.instructionsWebView.webViewClient = object : WebViewClient() {}
+            val websiteUrl: String = myBundle.sourceUrl
+            binding.instructionsWebView.loadUrl(websiteUrl)
+        }
+
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
